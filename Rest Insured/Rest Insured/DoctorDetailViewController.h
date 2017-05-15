f30c7e73889085a36bb54575a37344a7e15a1b68
David <davidporter33@icloud.com>
2017-05-15 16:09:11 -0700

    added doctor detail view controller, practice detail view controller, and search insurance view controller.

---
+++ Rest Insured/Rest Insured/DoctorDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DoctorDetailViewController.h
+//  Rest Insured
+//
+//  Created by David Porter on 5/15/17.
+//  Copyright © 2017 Brandon Little. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DoctorDetailViewController : UIViewController
+
+@end
---
+++ Rest Insured/Rest Insured/SearchInsuranceViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchInsuranceViewController.h
+//  Rest Insured
+//
+//  Created by David Porter on 5/15/17.
+//  Copyright © 2017 Brandon Little. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchInsuranceViewController : UIViewController
+
+@end
