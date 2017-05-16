50f61761087b9a0feddac4246f9f6891f48ea735
David <davidporter33@icloud.com>
2017-05-16 10:10:49 -0700

    Added user model with full name, email, provide and provideriD

---
+++ Rest Insured/Rest Insured/User.m
@@ -0,0 +1,34 @@
+//
+//  User.m
+//  Rest Insured
+//
+//  Created by David Porter on 5/16/17.
+//  Copyright © 2017 Brandon Little. All rights reserved.
+//
+
+#import "User.h"
+
+@interface User ()
+
+@property(strong, nonatomic)NSString *fullName;
+@property(strong, nonatomic)NSString *email;
+@property(strong, nonatomic)NSString *providerName;
+@property(strong, nonatomic)NSString *providerID;
+
+
+@end
+
+@implementation User
+
+-(instancetype)initWithName:(NSString *) fullName email:(NSString *) email providerName:(NSString *) providerName providerID:(NSString *) providerID {
+    
+    self.fullName = fullName;
+    self.email = email;
+    self.providerName = providerName;
+    self.providerID = providerID;
+    
+    return self;
+    
+}
+
+@end
